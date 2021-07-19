27d67c5368b16a618ab115bb4b2fb8067d1bb827
Jungea <jungea2241@gmail.com>
2021-07-20 00:27:12 +0900

    정적파일 추가(html, js)

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    return render_template("hello.html")
+
+
+@app.route('/api/')
+def api_index():
     return {'Hello': 'World!'}
 
 
-@app.route('/hello')
-def hello():
+@app.route('/api/hello')
+def api_hello():
     return '<h1>hello world</h1>'
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
