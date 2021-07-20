ecc315b222c3fe0c0e037798455af124af0140b2
Jungea <jungea2241@gmail.com>
2021-07-20 16:09:19 +0900

    value, for, if

--- main.py
+++ main.py
@@ -8,20 +8,21 @@ def login_page():
     return render_template('login.html')
 
 
-@app.route('/success/<name>')
-def success(name):
-    return 'Welcome %s' % name
+@app.route('/success/<name>/<int:auth>')
+def success_page(name, auth):
+    info = {'name': 'hana', 'age': 25, 'phone': '01012345678'}
+    return render_template('success.html', name=name, auth=auth, info=info)
 
 
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
     if request.method == 'POST':
         form_name = request.form['name']
-        return redirect(url_for('success', name=form_name))
+        return redirect(url_for('success_page', name=form_name, auth=200))
 
     else:
         query_name = request.args.get('name')
-        return redirect(url_for('success', name=query_name))
+        return redirect(url_for('success_page', name=query_name, auth=500))
 
 
 if __name__ == "__main__":
