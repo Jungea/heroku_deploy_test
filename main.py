b51efd90bae5aca4ede3e6ecfb30e427c2ec15b6
Jungea <jungea2241@gmail.com>
2021-07-20 15:35:50 +0900

    url_for, redirect

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
+
+
+##########################################################
+# HTML
+##########################################################
 
 
 @app.route('/')
 def index():
     return render_template("hello.html")
 
-
-@app.route('/api/')
-def api_index():
-    return {'Hello': 'World!'}
+##########################################################
+# API
+##########################################################
 
 
 @app.route('/api/hello')
@@ -18,5 +22,35 @@ def api_hello():
     return '<h1>hello world</h1>'
 
 
+# parameter
+@app.route('/api/hello/<username>')
+def api_string(username):
+
+    if username == 'world':
+        return redirect(url_for('api_hello'))  # redirect
+
+    return {'Hello': username}
+
+
+@app.route('/api/page/<int:pageId>')  # <float:no>
+def api_int(pageId):
+    return {'page is': pageId}
+
+##########################################################
+# etc
+##########################################################
+
+
+with app.test_request_context():
+    # ('함수이름', params=value)
+    print(url_for('api_string', username='hong'))
+
+
+##########################################################
+# run
+##########################################################
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+
+# https://roksf0130.tistory.com/94?category=780144
