02d21932b4417169e17bcf70b636787f68b7cdc8
Jungea <jungea2241@gmail.com>
2021-07-20 15:49:18 +0900

    request

--- main.py
+++ main.py
@@ -1,54 +1,28 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
 
-##########################################################
-# HTML
-##########################################################
+@app.route('/loginPage')
+def login_page():
+    return render_template('login.html')
 
 
-@app.route('/')
-def index():
-    return render_template("hello.html")
-
-##########################################################
-# API
-##########################################################
+@app.route('/success/<name>')
+def success(name):
+    return 'Welcome %s' % name
 
 
-@app.route('/api/hello')
-def api_hello():
-    return '<h1>hello world</h1>'
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        form_name = request.form['name']
+        return redirect(url_for('success', name=form_name))
 
+    else:
+        query_name = request.args.get('name')
+        return redirect(url_for('success', name=query_name))
 
-# parameter
-@app.route('/api/hello/<username>')
-def api_string(username):
-
-    if username == 'world':
-        return redirect(url_for('api_hello'))  # redirect
-
-    return {'Hello': username}
-
-
-@app.route('/api/page/<int:pageId>')  # <float:no>
-def api_int(pageId):
-    return {'page is': pageId}
-
-##########################################################
-# etc
-##########################################################
-
-
-with app.test_request_context():
-    # ('함수이름', params=value)
-    print(url_for('api_string', username='hong'))
-
-
-##########################################################
-# run
-##########################################################
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
